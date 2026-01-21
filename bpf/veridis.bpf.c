5404c69ca92d2c07036756623cd1de075ec2188f
kartikgoyal137 <kartikcodes137@gmail.com>
2026-01-21 14:22:16 +0530

    add eBPF Map to fetch cpu_time for every PID

--- bpf/veridis.bpf.c
+++ bpf/veridis.bpf.c
@@ -1,9 +1,42 @@
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 
+struct {
+  __uint(type, BPF_MAP_TYPE_HASH);
+  __uint(max_entries, 32768);
+  __type(key, u32);
+  __type(value, u64);
+} start_time SEC(".maps");
+
+struct {
+  __uint(type, BPF_MAP_TYPE_HASH);
+  __uint(max_entries, 32768);
+  __type(key, u32);
+  __type(value, u64);
+} cpu_time SEC(".maps");
+
 SEC("tracepoint/sched/sched_switch")
 int handle_sched_switch(struct trace_event_raw_sched_switch *ctx) {
-    bpf_printk("Switch happening\n");
-    return 0;
+  u64 now = bpf_ktime_get_ns();
+
+  u32 prev_pid = ctx->prev_pid;
+  u64 *start = bpf_map_lookup_elem(&start_time, &prev_pid);
+  if(start) {
+    u64 delta = now - *start;
+    u64 *total = bpf_map_lookup_elem(&cpu_time, &prev_pid);
+
+    if(total)
+      *total += delta;
+    else 
+      bpf_map_update_elem(&cpu_time, &prev_pid, &delta, BPF_ANY);
+
+    bpf_map_delete_elem(&start_time, &prev_pid);
+  }
+
+  u32 next_pid = ctx->next_pid;
+  bpf_map_update_elem(&start_time, &next_pid, &now, BPF_ANY);
+  
+
+  return 0;
 }
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
--- src/core/scheduler.cpp
+++ src/core/scheduler.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <unistd.h>
 #include <bpf/libbpf.h>
+#include <bpf/bpf.h>
 
 int main() {
   struct bpf_object *obj;
   struct bpf_program *prog;
   struct bpf_link *link;
+  struct bpf_map *map;
 
   obj = bpf_object__open_file("./target/veridis.bpf.o", nullptr);
   if(!obj) {
@@ -17,6 +19,13 @@ int main() {
     std::cerr << "Failed to load BPF bytecode\n";
     return 1;
   }
+
+  map = bpf_object__find_map_by_name(obj, "cpu_time");
+  if (!map) {
+    std::cerr << "Map cpu_time not found\n";
+    return 1;
+  }
+  int map_fd = bpf_map__fd(map);
 
   prog = bpf_object__find_program_by_name(obj, "handle_sched_switch");
   if(!prog) {
@@ -31,8 +40,22 @@ int main() {
   }
 
   std::cout << "eBPF program running";
-  pause();
 
+  while(1) {
+    sleep(1);
+    printf("------------\n");
+
+    uint32_t pid = 0; uint32_t next_pid;
+    uint64_t time;
+
+    while(bpf_map_get_next_key(map_fd, &pid, &next_pid) == 0) {
+      bpf_map_lookup_elem(map_fd, &next_pid, &time);
+      printf("PID %-6u CPU %.3f ms\n",
+                   next_pid, time / 1e6);
+      pid = next_pid;
+    }
+  }
+  
   bpf_link__destroy(link);
   bpf_object__close(obj);
 }
